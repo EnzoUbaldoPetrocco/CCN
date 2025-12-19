ad7b3207d0dded8c1d8a21344c5d90c383a48866
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2025-12-19 14:24:12 +0100

    Uploading data analysis

--- DataAnalysis/intro/introSeparated.py
+++ DataAnalysis/intro/introSeparated.py
@@ -55,7 +55,42 @@ label_map = {
             "Please rate your impression of the robot you just interacted with by selecting a point on the scale between the two adjectives. There are no right or wrong answers. .4": "Competent",
             "Please rate your impression of the robot you just interacted with by selecting a point on the scale between the two adjectives. There are no right or wrong answers. .5": "Knowledgable"
     }
-    
+
+question_sections = {
+    "culture_closeness_questions": [
+        "Which picture best describes your relationship with Italy or Germany?",
+        "Which picture best describes your relationship with Italian or German language?",
+        "Which picture best describes your relationship with Italian or German Culture?"
+    ],
+    "personality_questions": [
+        "I see myself as someone who  [... is reserved ]",
+        "I see myself as someone who  [... is generally trusting]",
+        "I see myself as someone who  [... tends to be lazy]",
+        "I see myself as someone who  [... is relaxed, handles stress well]",
+        "I see myself as someone who  [... has few artistic interests]",
+        "I see myself as someone who  [... is ongoing, sociable]",
+        "I see myself as someone who  [... tends to find fault with others]",
+        "I see myself as someone who  [... does a thorough job]",
+        "I see myself as someone who  [... get nervous easily]",
+        "I see myself as someone who  [... has active imagination]"
+    ],
+    "trust_questions": [
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Function successfully]",
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Act consistenly]",
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Reliable]",
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Predictable]",
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Dependable]",
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Follow directions]",
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Meet the needs of the mission]",
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Perform exactly as instructed]",
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Have errors]",
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Provide appropriate information]",
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Malfunction]",
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Communicate with people]",
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Provide Feedback]",
+        "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Unresponsive]"
+    ]
+}   
 
 label_to_keep = [
     "Which picture best describes your relationship with Italy or Germany?",
@@ -83,11 +118,11 @@ def clean_data(df):
     df = df.drop_duplicates()
     df = df.ffill().bfill()
     df = df.replace([np.inf, -np.inf], np.nan)
-    df = df.replace('Disagree strongly', 1)
-    df = df.replace('Disagree a little', 2)
-    df = df.replace('Neither agree or disagree', 3)
-    df = df.replace('Agree a little', 4)
-    df = df.replace('Agree strongly', 5)
+    df = df.replace('Disagree strongly', -2)
+    df = df.replace('Disagree a little', -1)
+    df = df.replace('Neither agree or disagree', 0)
+    df = df.replace('Agree a little', 1)
+    df = df.replace('Agree strongly', 2)
     df = df.replace('German', 0)
     df = df.replace('Italian', 1)
     def percent_to_float(x):
@@ -115,26 +150,6 @@ def clean_data(df):
     df["mean_trust_subj"] = (temp + temp2)/2
     return df
 
-def analyze_data(df, threshold=0.8):
-    """Perform basic analysis on the DataFrame and highlight correlations."""
-    summary = df.describe(include='all')
-    correlations = df.corr()
-
-    # Find pairs with strong correlation
-    correlated_pairs = (
-        correlations
-        .abs()                              # absolute correlation
-        .unstack()                          # flatten into Series
-        .sort_values(ascending=False)       # sort high → low
-    )
-
-    # Remove self-correlations (always 1.0 on diagonal)
-    correlated_pairs = correlated_pairs[correlated_pairs < 1]
-
-    # Keep only those above threshold
-    strong_corrs = correlated_pairs[correlated_pairs.abs() > threshold]
-
-    return summary, correlations, strong_corrs
 
 def annotate_with_stars(r_val, p_val):
     """
@@ -168,11 +183,12 @@ def generate_correlation_heatmaps(df, pt=""):
         df (pd.DataFrame): The input DataFrame containing all data.
         group_col (str): The column name used for grouping (e.g., 'P').
     """
-    df = df[[col for col in label_to_keep if col in df.columns]].copy()
+    #df = df[[col for col in label_to_keep if col in df.columns]].copy()
+    df.rename(columns=label_map, inplace=True)
         # 1. Subsetting and Preprocessing
         
     # Drop specified non-feature columns
-    subset = df.drop(columns=["Participant ID", "Nationality"], errors="ignore")
+    subset = df.drop(columns=["Participant ID"], errors="ignore")
 
     if subset.empty:
         return
@@ -226,8 +242,8 @@ def generate_correlation_heatmaps(df, pt=""):
         for j in cols:
             annotation_df.loc[i, j] = annotate_with_stars(r_matrix.loc[i, j], p_matrix.loc[i, j])
 
-    print(f"R-Matrix:\n{r_matrix.round(3).head(3)}")
-    print(f"Annotation Matrix:\n{annotation_df.head(3)}")
+    #print(f"R-Matrix:\n{r_matrix.round(3).head(3)}")
+    #print(f"Annotation Matrix:\n{annotation_df.head(3)}")
 
     # 4. Generate Heatmap
     
@@ -247,16 +263,35 @@ def generate_correlation_heatmaps(df, pt=""):
         linecolor='white',
         cbar_kws={'label': 'Pearson Correlation Coefficient (r)'}
     )
-    
     title = f"Pearson Correlation Heatmap"
     plt.title(title, fontsize=16)
     plt.tight_layout()
-    
     # Save the figure
     filename = f"./{pt}Pearson_Correlation_Heatmap.png"
     plt.savefig(filename, dpi=300)
     plt.close()
+    print(f"Successfully generated and saved: {filename}")
 
+    plt.figure(figsize=(12, 10))
+    sns.heatmap(
+        p_matrix,            # 1. Numeric data for colors (P values)
+        mask=mask,
+        annot=True,         # 2. Annotate with P values
+        fmt=".3f",          # Format P values to three decimal places
+        cmap="YlGnBu_r",    # Reverse colormap for better visibility
+        vmin=0,
+        vmax=0.05,
+        linewidths=0.5,
+        linecolor='white',
+        cbar_kws={'label': 'P-Value'}
+    )
+    title = f"P-Value Heatmap"
+    plt.title(title, fontsize=16)
+    plt.tight_layout()
+    # Save the figure
+    filename = f"./{pt}P_Value_Heatmap.png"
+    plt.savefig(filename, dpi=300)
+    plt.close()
     print(f"Successfully generated and saved: {filename}")
 
 
@@ -268,108 +303,38 @@ if __name__ == "__main__":
     file_path = "./Intro CCN  (Risposte).CSV"
     data = load_data(file_path)
     cleaned_data = clean_data(data)
-
-    label_map = {
-            # Closeness questions
-            "Which picture best describes your relationship with Italy or Germany?": "Closeness_Country",
-            "Which picture best describes your relationship with Italian or German language?": "Closeness_Language",
-            "Which picture best describes your relationship with Italian or German Culture?": "Closeness_Culture",
-
-            # Personality (OCEAN 10)
-            "I see myself as someone who  [... is reserved ]": "Reserved",
-            "I see myself as someone who  [... is generally trusting]": "Trusting",
-            "I see myself as someone who  [... tends to be lazy]": "Lazy",
-            "I see myself as someone who  [... is relaxed, handles stress well]": "Relaxed",
-            "I see myself as someone who  [... has few artistic interests]": "Artistic",
-            "I see myself as someone who  [... is ongoing, sociable]": "Sociable",
-            "I see myself as someone who  [... tends to find fault with others]": "Critical",
-            "I see myself as someone who  [... does a thorough job]": "Thorough",
-            "I see myself as someone who  [... get nervous easily]": "Neurotic",
-            "I see myself as someone who  [... has active imagination]": "Imaginative",
-
-            # Trust in Technology (Trust 14 items)
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Function successfully]": "Function",
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Act consistenly]": "Consistent",
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Reliable]": "Reliable",
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Predictable]": "Predictable",
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Dependable]": "Dependable",
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Follow directions]": "Follow_Directions",
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Meet the needs of the mission]": "Mission",
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Perform exactly as instructed]": "Perform",
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Have errors]": "Errors",
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Provide appropriate information]": "Info",
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Malfunction]": "Malfunction",
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Communicate with people]": "Communicate",
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Provide Feedback]": "Feedback",
-            "Consider expectations toward Pepper robot and express how much do you agree with the properties attributed to it (scale: 0%-100%, scroll in to see all the options).  [Unresponsive]": "Unresponsive"
-        }
-
-
-    summary, correlations, strong_corrs = analyze_data(cleaned_data)
-
-    corr_df_short = correlations.rename(columns=label_map, index=label_map)
-    mask = np.triu(np.ones_like(corr_df_short, dtype=bool))
-    plt.figure(figsize=(20, 16))
-    sns.heatmap(corr_df_short, mask=mask, annot=True, fmt=".1f", cmap="coolwarm", vmin=-1, vmax=1)
-    plt.title("Correlation Matrix Heatmap (Upper Triangle Hidden)")
-    plt.tight_layout()
-    plt.savefig("correlation_heatmap_upper.png", dpi=300)
-    plt.close()
-    
-    #print("Data Summary:")
-    #print(summary)
-    summary.to_csv("data_intro_summary.csv")
-    #print("\nCorrelations:")
-    #print(correlations)
-    correlations.to_csv("data_intro_correlations.csv")
-    print("\nStrong Correlations (>|0.8|):")
-    print(strong_corrs)
-
     german_data = take_only_one_culture(cleaned_data, culture=0)
-    german_summary, german_correlations, german_strong_corrs = analyze_data(german_data)
-
-    #print("\nGerman Data Summary:")
-    #print(german_summary)
-    german_summary.to_csv("data_intro_german_summary.csv")
-    #print("\nGerman Correlations:")
-    #print(german_correlations)
-    german_correlations.to_csv("data_intro_german_correlations.csv")
-    print("\nStrong Correlations in German Data (>|0.8|):")
-    print(german_strong_corrs)
-
-    corr_df_short = german_correlations.rename(columns=label_map, index=label_map)
-    mask = np.triu(np.ones_like(corr_df_short, dtype=bool))
-    plt.figure(figsize=(20, 16))
-    sns.heatmap(corr_df_short, mask=mask, annot=True, fmt=".1f", cmap="coolwarm", vmin=-1, vmax=1)
-    plt.title("Correlation Matrix Heatmap (Upper Triangle Hidden)")
-    plt.tight_layout()
-    plt.savefig("correlation_heatmap_german_upper.png", dpi=300)
-    plt.close()
-
-
-
     italian_data = take_only_one_culture(cleaned_data, culture=1)
-    italian_summary, italian_correlations, italian_strong_corrs = analyze_data(italian_data)
-    #print("\nItalian Data Summary:")
-    #print(italian_summary)
-    italian_summary.to_csv("data_intro_italian_summary.csv")
-    #print("\nItalian Correlations:")
-    #print(italian_correlations)
-    italian_correlations.to_csv("data_intro_italian_correlations.csv")
-    print("\nStrong Correlations in Italian Data (>|0.8|):")
-    print(italian_strong_corrs)
 
-    corr_df_short = italian_correlations.rename(columns=label_map, index=label_map)
-    mask = np.triu(np.ones_like(corr_df_short, dtype=bool))
-    plt.figure(figsize=(20, 16))
-    sns.heatmap(corr_df_short, mask=mask, annot=True, fmt=".1f", cmap="coolwarm", vmin=-1, vmax=1)
-    plt.title("Correlation Matrix Heatmap (Upper Triangle Hidden)")
-    plt.tight_layout()
-    plt.savefig("correlation_heatmap_italian_upper.png", dpi=300)
-    plt.close()
+    #generate_correlation_heatmaps(cleaned_data, pt="Overall_")
+    #generate_correlation_heatmaps(german_data, pt="German_")
+    #generate_correlation_heatmaps(italian_data, pt="Italian_")
 
+    print("Data Cleaned columns")
+    print(cleaned_data.columns)
 
-    generate_correlation_heatmaps(cleaned_data, pt="Overall_")
-    generate_correlation_heatmaps(german_data, pt="German_")
-    generate_correlation_heatmaps(italian_data, pt="Italian_")
+    questions_culturevspersonality = question_sections["culture_closeness_questions"] + question_sections["personality_questions"]
+    clean_data_culturevspersonality = cleaned_data[questions_culturevspersonality].copy()
+    german_data_culturevspersonality = german_data[questions_culturevspersonality].copy()
+    italian_data_culturevspersonality = italian_data[questions_culturevspersonality].copy()
+    generate_correlation_heatmaps(german_data_culturevspersonality, pt="German_CultureVsPersonality_")
+    generate_correlation_heatmaps(italian_data_culturevspersonality, pt="Italian_CultureVsPersonality_")
+    generate_correlation_heatmaps(clean_data_culturevspersonality, pt="CultureVsPersonality_")
 
+    questions_culturevstrust = question_sections["culture_closeness_questions"] + question_sections["trust_questions"]
+    clean_data_culturevstrust = cleaned_data[questions_culturevstrust].copy()
+    german_data_culturevstrust = german_data[questions_culturevstrust].copy()
+    italian_data_culturevstrust = italian_data[questions_culturevstrust].copy()
+    generate_correlation_heatmaps(german_data_culturevstrust, pt="German_CultureVsTrust_")
+    generate_correlation_heatmaps(italian_data_culturevstrust, pt="Italian_CultureVsTrust_")
+    generate_correlation_heatmaps(clean_data_culturevstrust, pt="CultureVsTrust_")
+
+    questions_personalityvstrust = question_sections["personality_questions"] + question_sections["trust_questions"]
+    clean_data_personalityvstrust = cleaned_data[questions_personalityvstrust].copy()
+    german_data_personalityvstrust = german_data[questions_personalityvstrust].copy()
+    italian_data_personalityvstrust = italian_data[questions_personalityvstrust].copy()
+    generate_correlation_heatmaps(german_data_personalityvstrust, pt="German_PersonalityVsTrust_")
+    generate_correlation_heatmaps(italian_data_personalityvstrust, pt="Italian_PersonalityVsTrust_")
+    generate_correlation_heatmaps(clean_data_personalityvstrust, pt="PersonalityVsTrust_")
+
+    
